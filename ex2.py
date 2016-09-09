d00e213e3a242b9f7ef2d38b68f67875c507ce5c
jibin jose <jibin.jacob13@gmail.com>
2016-09-09 15:25:05 +0530

    comments in ppython

---
+++ ex2.py
@@ -0,0 +1,9 @@
+# A comment, this so you can read program lator.
+# Anything after the # is ignored by python
+
+print("I could have code like this.")
+
+# You can also use a comment to "disable" or comment out a piece of code
+# print("this won't run")
+
+print("This will run.")
